f138d2676b67f97355af178d9cb873a7c8df80db
k.tavares.martins <k.tavares.martins@accenture.com>
2024-09-20 16:03:52 -0300

    change request

--- src/parseRequest/parseRequest.schema.js
+++ src/parseRequest/parseRequest.schema.js
@@ -1,12 +1,31 @@
 const schema = {
   type: 'object',
   additionalProperties: false,
-  required: ['id', 'origin', 'email', 'planId'],
+  required: [
+    'id',
+    'origin',
+    'email',
+    'frequency',
+    'frequencyType',
+    'reason',
+    'backUrl',
+    'amount',
+  ],
   properties: {
     id: { type: 'string', minLength: 1, maxLength: 255 },
     origin: { type: 'string', minLength: 1, maxLength: 255 },
     email: { type: 'string', minLength: 1, maxLength: 255 },
-    planId: { type: 'string', minLength: 1, maxLength: 255 },
+    frequency: {
+      type: 'number',
+      minimum: 1,
+      maximum: 12,
+      multipleOf: 1,
+    },
+    frequencyType: { type: 'string', minLength: 1, maxLength: 255 },
+    reason: { type: 'string', minLength: 1, maxLength: 255 },
+    amount: { type: 'number', minimum: 1, maximum: 255 },
+    backUrl: { type: 'string', minLength: 1, maxLength: 255 },
+    token: { type: 'string', minLength: 1, maxLength: 255 },
   },
 };
 
--- src/sendDynamoDbRequest/sendDynamoDbRequest.js
+++ src/sendDynamoDbRequest/sendDynamoDbRequest.js
@@ -5,15 +5,18 @@ const dynamodb = new DynamoDB();
 const sendDynamoDbRequest = async (content, gatewayId) => {
   await dynamodb
     .putItem({
-      TableName: `hub-payment-subscription-${process.env.AWS_ENV}`,
+      TableName: `hub-payment-subscriptions-${process.env.AWS_ENV}`,
       Item: {
         token: { S: content.token },
         originId: { S: `${content.origin}|${content.id}` },
         createdAt: { S: `${new Date().toISOString()}` },
-        planId: { S: content.planId },
+        frequency: { S: `${content.frequency}` },
+        frequencyType: { S: content.frequencyType },
+        amount: { S: `${content.amount}` },
         request: { S: JSON.stringify(content) },
         gatewayId: { S: `mercadopago|${gatewayId}` },
         email: { S: content.email },
+        status: { S: 'pending' },
       },
     })
     .promise();
